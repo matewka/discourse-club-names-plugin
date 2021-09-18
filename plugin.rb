9fd8a6cf1f5981fa99d24f4d962ca6169f161b19
Mateusz Gawlinski <mateusz.gawlinski@twill.net>
2021-09-18 12:36:14 +0200

    Added plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,42 @@
+# name: club-names-plugin
+# about: Plugin for displaying user full names only to members of the same club (group)
+# version: 0.0.1
+# authors: CelPal
+
+after_initialize {
+  require_dependency 'basic_user_serializer'
+
+  class ::BasicUserSerializer
+    attributes :name
+
+    def name
+      if user_is_current_user
+        return _old_name_method
+      else
+        # TODO: make this list editable
+        clubGroups = Array['ksi']
+
+        if scope.user
+          ownGroups = GroupUser.where(user_id: scope.user.id)
+                               .map { |gu| gu.group_id }
+                               .map { |group_id| Group.find_by(id: group_id) }
+                               .filter { |group| clubGroups.include? group.name }
+
+          if ownGroups.length > 0
+            ownGroupsIds = ownGroups.map { |g| g.id }
+
+            if GroupUser.exists?(user_id: user.id, group_id: ownGroupsIds)
+              return _old_name_method
+            end
+          end
+        end
+      end
+
+      return ''
+    end
+
+    private def _old_name_method
+      return Hash === user ? user[:name] : user.try(:name)
+    end
+  end
+}
